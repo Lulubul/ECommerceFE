82e124b6394f2096ddec7ea6fbff00ad1451ff23
unknown <didas_convict@yahoo.com>
2015-08-08 17:25:05 +0300

    Project structure generated with yo angular.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ecommerceFrontEndApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the ecommerceFrontEndApp
+ */
+angular.module('ecommerceFrontEndApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ecommerceFrontEndApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ecommerceFrontEndApp
+ */
+angular.module('ecommerceFrontEndApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
